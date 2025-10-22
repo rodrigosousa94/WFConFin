4efbfeae4b86e509f554831570265ff217dd7684
Rodrigo <r.oliveira5117@gmail.com>
2025-10-21 22:04:15 -0300

    jwt

--- WFConFin/Program.cs
+++ WFConFin/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WFConFin.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,31 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var chave = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("Chave").Get<string>());
+
+builder.Services.AddAuthentication(
+    x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(
+    x => 
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(chave),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    }
+);
+
+
 
 var app = builder.Build();
 
